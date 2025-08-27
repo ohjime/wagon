183f450d89751b85bdf1007b68bf6cd1492e4bb4
ajimee <afnanjime123@gmail.com>
2025-08-26 19:01:19 -0600

    Htmx Filter Search Bar

--- src/server/lib/core/views.py
+++ src/server/lib/core/views.py
@@ -3,6 +3,7 @@ from .models import Trip
 from .tables import TripTable
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.db.models import Q
 
 
 def trips_index(request):
@@ -13,6 +14,26 @@ def trips_index(request):
     else:
         template_name = "core/trips/trips.html"
     return render(request, template_name, {"table": table})
+
+
+# Search/filtering of trips table
+def table_htmx(request):
+    import time
+
+    time.sleep(1)
+    query = request.GET.get("search", "").strip()
+    qs = Trip.objects.all()
+    if query:
+        qs = qs.filter(
+            Q(id__icontains=query)
+            | Q(rider__account__name__icontains=query)
+            | Q(driver__account__name__icontains=query)
+            | Q(origin__icontains=query)
+            | Q(destination__icontains=query)
+        )
+    table = TripTable(qs)
+    RequestConfig(request, paginate={"per_page": 80}).configure(table)  # type: ignore
+    return render(request, "core/trips/partials/list.html", {"table": table})
 
 
 def riders_index(request):
